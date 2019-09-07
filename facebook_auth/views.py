228907e08f5cc352a3675934caef924692011255
JunHyeok Jung <9913103+CheeseStick@users.noreply.github.com>
2019-09-07 20:06:58 +1200

    **[ADD] Added User profile check**

--- facebook_auth/views.py
+++ facebook_auth/views.py
@@ -1,8 +1,18 @@
+import json
+
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 def login(request):
     return render(request, 'facebook_auth/login.html')
+
+@login_required
+def get_user_info(request):
+    if not request.user.is_authenticated():
+        return render(request, 'facebook_auth/login.html')
+    return HttpResponse(json.dumps(request.backend.strategy.request_data()), content_type="application/json")
+
 
 #@login_required
 def index(request):
