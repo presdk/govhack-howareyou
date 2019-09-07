5e3b24fc9542c369777c421f4b81f5ec0e65fce9
presdk <dawinkweon@gmail.com>
2019-09-07 12:44:34 +1200

    fix(login): styling

--- facebook_auth/views.py
+++ facebook_auth/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 def login(request):
     return render(request, 'facebook_auth/login.html')
 
+
 @login_required
 def home(request):
     return render(request, 'home.html')
