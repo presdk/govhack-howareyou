afee1831be2a07dbe7099498051a9e6c4a5f3fd4
presdk <dawinkweon@gmail.com>
2019-09-07 17:04:07 +1200

    fix: remove unrequired routes

--- facebook_auth/views.py
+++ facebook_auth/views.py
@@ -4,15 +4,6 @@ from django.contrib.auth.decorators import login_required
 def login(request):
     return render(request, 'facebook_auth/login.html')
 
-
 #@login_required
 def index(request):
     return render(request, 'pages/index.html')
-    
-#@login_required
-def assessment(request):
-    return render(request, "pages/assessment.html")
-
-#@login_required
-def completed(request):
-    return render(request, "pages/completed.html")
