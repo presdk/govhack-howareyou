98d53477391240c871725651c3ec28e14be59242
JunHyeok Jung <9913103+CheeseStick@users.noreply.github.com>
2019-09-08 15:37:08 +1200

    123123

--- facebook_auth/views.py
+++ facebook_auth/views.py
@@ -1,21 +1,19 @@
-import json
-
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render
 
 def login(request):
     return index(request)
 
+
 def get_user_info(request):
     if request.user.is_authenticated:
         social_user = request.user.social_auth.get()
-        return HttpResponse(status=200,
-                            content_type="application/json",
-                            content=json.dumps(social_user.extra_data))
+        return JsonResponse(status=200,
+                            data=social_user.extra_data)
     else:
-        return HttpResponse(status=403,
-                            content_type="application/json",
-                            content={"error": "User is not authenticated."})
+        return JsonResponse(status=403,
+                            data={"error": "User is not authenticated."})
+
 
 def index(request):
     return render(request, 'pages/index.html')
