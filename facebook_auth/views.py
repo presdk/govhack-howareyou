979eb9c16d0428263476af214a7220a29a48265d
JunHyeok Jung <9913103+CheeseStick@users.noreply.github.com>
2019-09-08 12:19:09 +1200

    .

--- facebook_auth/views.py
+++ facebook_auth/views.py
@@ -1,9 +1,7 @@
 import json
 
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
+from django.shortcuts import render
 
 def login(request):
     return index(request)
@@ -13,7 +11,7 @@ def get_user_info(request):
         social_user = request.user.social_auth.get()
         return HttpResponse(json.dumps(social_user.extra_data), content_type="application/json")
     else:
-        return HttpResponse(status_code=403,
+        return HttpResponse(code=403,
                             content_type="application/json",
                             content={"error": "User is not authenticated."})
 
